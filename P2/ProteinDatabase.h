8df124ac70652d9692d7f451017fa5d1d62f570c
Aryaman Giri <aryamankgiri@gmail.com>
2024-05-26 22:05:16 +0545

    Updated .h file

--- P2/ProteinDatabase.h
+++ P2/ProteinDatabase.h
@@ -2,7 +2,6 @@
 #define SENECA_PROTEINDATABASE_H
 
 #include <string>
-#include <fstream>
 #include <vector>
 
 namespace seneca {
@@ -12,63 +11,21 @@ class ProteinDatabase {
 
 public:
     ProteinDatabase() = default;
-
-    ProteinDatabase(const std::string& filename) {
-        std::ifstream file(filename);
-        if (file.is_open()) {
-            std::string line;
-            std::string sequence;
-            while (getline(file, line)) {
-                if (line[0] == '>') {
-                    if (!sequence.empty()) {
-                        sequences.push_back(sequence);
-                        sequence.clear();
-                    }
-                } else {
-                    sequence += line;
-                }
-            }
-            if (!sequence.empty()) {
-                sequences.push_back(sequence);
-            }
-        }
-    }
-
-    size_t size() const {
-        return sequences.size();
-    }
-
-    std::string operator[](size_t index) const {
-        if (index < sequences.size()) {
-            return sequences[index];
-        } else {
-            return "";
-        }
-    }
+    ProteinDatabase(const std::string& filename);
+    size_t size() const;
+    std::string operator[](size_t index) const;
 
     // Copy constructor
-    ProteinDatabase(const ProteinDatabase& other)
-        : sequences(other.sequences) {}
+    ProteinDatabase(const ProteinDatabase& other);
 
     // Copy assignment operator
-    ProteinDatabase& operator=(const ProteinDatabase& other) {
-        if (this != &other) {
-            sequences = other.sequences;
-        }
-        return *this;
-    }
+    ProteinDatabase& operator=(const ProteinDatabase& other);
 
     // Move constructor
-    ProteinDatabase(ProteinDatabase&& other) noexcept
-        : sequences(std::move(other.sequences)) {}
+    ProteinDatabase(ProteinDatabase&& other) noexcept;
 
     // Move assignment operator
-    ProteinDatabase& operator=(ProteinDatabase&& other) noexcept {
-        if (this != &other) {
-            sequences = std::move(other.sequences);
-        }
-        return *this;
-    }
+    ProteinDatabase& operator=(ProteinDatabase&& other) noexcept;
 
     // Destructor
     ~ProteinDatabase() = default;
